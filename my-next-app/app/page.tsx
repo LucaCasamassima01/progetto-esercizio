733af5c274ea4ba789971869afbc4de8cec4d098
LucaCasamassima01 <luca.casamassima@codestorm.it>
2025-04-30 09:58:34 +0200

    Stencil package install

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -1,5 +1,12 @@
+'use client';
 import Link from "next/link";
+import { useEffect } from 'react';
 export default function Home() {
+  
+  useEffect(() => {
+  }, []);
+  
+  
   return (
     <>
       <header className="flex justify-center text-5xl text-white pt-20">
@@ -8,6 +15,9 @@ export default function Home() {
       <main className="text-center text-white pt-15 text-2xl cursor-pointer">
         <Link href="/inner/ssrtest">Vai al test...</Link>
       </main>
+      <footer className="text-center text-white pt-15 text-2xl cursor-pointer">
+        <stencil-button>Stencil button</stencil-button>
+      </footer>
     </>
   );
 }
